82b9971fae893d042d59815bd4ea9b28bbab5238
SameenFatima <sameen.fatima993@gmail.com>
2025-06-29 08:15:14 +0500

    splice learning

---
+++ 8th lesosn chap-17/splice.js
@@ -0,0 +1,18 @@
+// // arry mathode 4 //
+// var color = ["green" , "blue" , "red" , "indiago" , "pink"]
+// 1, first value green its called target index 
+
+// 2, second value as ky sath agy kitni delet krni hy 
+
+
+// color.splice (1, 2,)
+
+// console.log(color);
+
+// color.splice (1, 2, "yellow", "violate")
+// 3rd and 4th value incert krti hy  un ki jaga pr jin ko remove kia hy
+
+// console.log(color);
+
+
+
