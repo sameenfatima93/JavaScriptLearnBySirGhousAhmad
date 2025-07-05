612384436a151b03a4f715865c243549fd6731f9
SameenFatima <sameen.fatima993@gmail.com>
2025-07-06 04:12:00 +0500

    for_loop

---
+++ 3rd lesson task/code.js
@@ -0,0 +1,15 @@
+// task-1
+// var age = 30
+// alert ("i'm 30 years old")
+//-----------------------------------
+
+//task-2
+// var visitedTime = 21
+// alert("“You have visited this site N times”")
+
+//-----------------------------------
+//task-3
+ 
+var birthYear = "my birth year is 1993"
+document.write("my birth year is 1990")
+document.write( typeof birthYear )
