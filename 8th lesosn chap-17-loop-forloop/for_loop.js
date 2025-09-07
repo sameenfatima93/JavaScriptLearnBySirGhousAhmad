cf958e1eae6eb8332fb22202f5a5dbfccc43966a
SameenFatima <sameen.fatima993@gmail.com>
2025-09-07 15:49:27 +0500

    missing number finding prectice

--- 8th lesosn chap-17-loop-forloop/for_loop.js
+++ 8th lesosn chap-17-loop-forloop/for_loop.js
@@ -4,11 +4,11 @@
 
 // for (var i = 0; i < 7; i++){
 //     console.log(i);
-    
-    // jb tk loop chly ga us ki condition last pr fals hogi to loop rok jaye ga 6 pr rok jaye ga 
-    // break 
-    // break jb loop ko milta hy woh wahen rok jata hy bilkol aagy nhi chalta 
-    
+
+// jb tk loop chly ga us ki condition last pr fals hogi to loop rok jaye ga 6 pr rok jaye ga 
+// break 
+// break jb loop ko milta hy woh wahen rok jata hy bilkol aagy nhi chalta 
+
 // }
 // console.log(i); 
 // lekin jb loop ky bahar console hy us me i ki velue 7 aaye gi kiune ky jahan loop roka woh last 7 thi loop nhi chly ga baahr wala consol chaly ga 
@@ -26,15 +26,15 @@
 // task plandiram words jo sedha prho ya ulta same hi parha jaata hy
 
 // var word = prompt("Enter word");
-   
+
 // for(var i = 0 ; i < word.length; i++){
 
 
-    // console.log(i);
-    //let explan agr consol.log (i); krty to jo prompt me name aya hy us ki sari indexing bta dya jesy 'sameen' to lop ny chl kr bta dia 0 sy 5 tk number 6 nhi btaya kiunke 6 pr conditio fals ho rhi hy
+// console.log(i);
+//let explan agr consol.log (i); krty to jo prompt me name aya hy us ki sari indexing bta dya jesy 'sameen' to lop ny chl kr bta dia 0 sy 5 tk number 6 nhi btaya kiunke 6 pr conditio fals ho rhi hy
 
-    // console.log(word[i]); 
-    // ye kia to us ny har alphabet ko alg line me likh diya 
+// console.log(word[i]); 
+// ye kia to us ny har alphabet ko alg line me likh diya 
 // }
 
 //now hum ko ab name ky alphabet ulty chahiye last wala sb sy uper is ko kehty hen reverse letter
@@ -60,8 +60,8 @@
 // var reverse = ""
 // for (var i = palandrom.length - 1 ; i >= 0 ; i--) {
 //     reverse += palandrom[i]
-    
-    
+
+
 // }
 // // console.log(reverse);
 // if (palandrom === reverse) {
@@ -69,7 +69,7 @@
 
 // }else{
 //     console.log("it's not a palandrom");
-    
+
 // }
 
 // var number = [23,50,60,34,12,67,80]
@@ -77,33 +77,33 @@
 // for (let i = 0; i < number.length; i++) {
 //     if (minNumber > number[i] ) {
 //         minNumber = number[i]
-        
+
 //     }
-    
+
 // }
 // console.log(minNumber);
 
-var number = [23,50,60,34,12,67,80]
-maxNumber = number[0]
-for (let i = 0; i < number.length; i++) {
-    if (maxNumber < number[i] ) {
-        maxNumber = number[i]
-        
-    }
-    
-}
-console.log(maxNumber);
+// var number = [23,50,60,34,12,67,80]
+// maxNumber = number[0]
+// for (let i = 0; i < number.length; i++) {
+//     if (maxNumber < number[i] ) {
+//         maxNumber = number[i]
+
+//     }
+
+// }
+// console.log(maxNumber);
 
 // console.log(reverse);
- //loop bnany ky bad ab humen if ki condition lgani hy
-  
+//loop bnany ky bad ab humen if ki condition lgani hy
+
 //  if(word === reverse){
 //     console.log("it's a palindrom")
 //  }
 // else{
 //     console.log("it's not a palindrom")
 // }
- 
+
 
 
 
@@ -119,10 +119,10 @@ console.log(maxNumber);
 
 // if(word === reverse ){
 //     console.log("it is a plandirom ");
-    
+
 // }else{
 //     console.log("it's not a plandirom ");
-    
+
 // }
 
 // arry men sy min or max fine krna hy
@@ -131,19 +131,19 @@ console.log(maxNumber);
 //  var arr  = [12, 65 , 99 , 43 , 10 , 11 , 98 ] 
 //  var minNum = arr[0];
 //  var maxNum = arr[0];
- // dosry var men ksi bhi ek number ko arry ky assign krna hy 
+// dosry var men ksi bhi ek number ko arry ky assign krna hy 
 
 //  for(var i = 0 ; i < arr.length ; i++){
-    // if( minNum > arr[i]){
-    //     minNum = arr[i]
-    // }
+// if( minNum > arr[i]){
+//     minNum = arr[i]
+// }
 //     if( maxNum < arr[i]){
 //         maxNum = arr[i]
 //     }
 //  }
 //  console.log(maxNum);
- 
- // final syntex minimum number finding 
+
+// final syntex minimum number finding 
 
 //  var arr  = [12, 65 , 99 , 43 , 10 , 11 , 98 ] 
 //  var minNum = arr[0]
@@ -170,19 +170,19 @@ console.log(maxNumber);
 // var arr = [11 , 12 , 14 , 15 , 16 , 17 , 18 , 19 , 20]
 // for (var i = 0 ; i < arr.length; i++){
 
-    // console.log(arr[i] );
-    //simple itna kia to console me pori line sy series aa gaye ab agr chahty hen ky do number brabar me aayen to  is tarha likhna hy 
+// console.log(arr[i] );
+//simple itna kia to console me pori line sy series aa gaye ab agr chahty hen ky do number brabar me aayen to  is tarha likhna hy 
 
-    // console.log(arr[i] , arr[i + 1]);
+// console.log(arr[i] , arr[i + 1]);
 // 
-    // is ka answer un aaye ga ek line me 11 12 phir 12 13 do do number kr ky 
-    // ab agr ye janna ho ky kon a number missing hy to minus kia jaye ga 11 - 12 = 1  12 - 14 = 2 to jaan mius krny pr 1 aaye ga woh thek hy or jahan 2 answer me aa raha hy woh galat hy us ky liye arr[i +1] or arr[i] ky drmiyan koma hata kr minus krna hy arr[i+1] - arr[i]  to jawab me jahan 1 ana hy aaye gaor jahan 2 answer jawba aaye ga woh show ho jae ga
+// is ka answer un aaye ga ek line me 11 12 phir 12 13 do do number kr ky 
+// ab agr ye janna ho ky kon a number missing hy to minus kia jaye ga 11 - 12 = 1  12 - 14 = 2 to jaan mius krny pr 1 aaye ga woh thek hy or jahan 2 answer me aa raha hy woh galat hy us ky liye arr[i +1] or arr[i] ky drmiyan koma hata kr minus krna hy arr[i+1] - arr[i]  to jawab me jahan 1 ana hy aaye gaor jahan 2 answer jawba aaye ga woh show ho jae ga
 // console.log( arr [i + 1] - arr[i]);
 // ab 2 pr conditon lagani hy jb 2 aaye to pta chaly kon sa number missing hy  us ky liye 
 
 // if(arr [i + 1] - arr[i] === 2){
-    // console.log(arr[i]);
-    // is ky jwab me aaye ga woh number jis ko aagy waly sy plus krny pr 2 aya tha jesy yahan aaye ga 12 ab missing ky liye 12 + 1 kr do to missing word mil jaye ga
+// console.log(arr[i]);
+// is ky jwab me aaye ga woh number jis ko aagy waly sy plus krny pr 2 aya tha jesy yahan aaye ga 12 ab missing ky liye 12 + 1 kr do to missing word mil jaye ga
 //     console.log(arr[i] + 1);  
 // }
 // }
@@ -219,4 +219,47 @@ console.log(maxNumber);
 //         console.log(arr[i] + j); 
 //     }
 // }
- //ye formula b ka hy jb series ky ander ek sy ziada missing hon series sy bahar waly pr nhi chly ga
+//ye formula b ka hy jb series ky ander ek sy ziada missing hon series sy bahar waly pr nhi chly ga
+
+//_____prectice____
+
+// var number = [11, 13,14,17,18,20];
+//  var missingNum = [];
+// for (var i = 0; i < number.length; i++) {
+//     var diff = number[i + 1] - number[i];
+//      if (diff > 1) {
+//         for(var j=1 ; j < diff; j++ ){
+//             // console.log(number[i] + j);
+//             // missingNum.push(number[i] + j)
+//             number.splice( i+j , 0 ,number[i] + j)
+            
+//         }
+        
+//     }
+// }
+// console.log(number)
+
+//____agr first 0index pr missing ho to ye kren gy________//
+
+var number = [13, 14, 17, 18, 20];
+var start = 11; // starting number jahan se sequence hona chahiye
+var missingNum = [];
+
+// Step 1: check agar first element hi bada hai
+if (number[0] > start) {
+  for (var k = start; k < number[0]; k++) {
+    number.unshift(k); // missing ko start me insert karo
+  }
+}
+
+// Step 2: beech ke missing numbers fill karo
+for (var i = 0; i < number.length; i++) {
+  var diff = number[i + 1] - number[i];
+  if (diff > 1) {
+    for (var j = 1; j < diff; j++) {
+      number.splice(i + j, 0, number[i] + j);
+    }
+  }
+}
+
+console.log(number);
--- 9th lesson chac-18/app.js
+++ 9th lesson chac-18/app.js
@@ -1,4 +1,23 @@
 //____revision of palandrom taask
+//___formula to remember this Reverse string  → Compare  → Print 
+
+// var userName = prompt("enter any name and check who is palandrom and who is not")
+// var output =""
+
+// for(var i = userName.length -1  ; i >= 0; i--) // yahan string reverse ho raha hy
+  // {
+  // output += userName[i] //yahan porany ky sath naya store ho raha hy take sb ek sath likhy aayen
+  
+// }
+// console.log(output)
+//  if (userName.toLowerCase() === output.toLowerCase()) // yahan compare ho raha hy 
+//   {
+//   console.log("this is a palandrom")
+//  }else{
+//   console.log("this is not palandrom");  
+//  }
+
+//____revision += formula
 
 // var str = null;
 //  str += "Saylani"
@@ -26,17 +45,17 @@
 
 // fixed missing number in arry series by sir Ghous Ahmad
 
-var arr =  [11,12,13,15,16,18,20] 
+// var arr =  [11,12,13,15,16,18,20] 
 
-for (var i = 0 ; i < arr.length; i++) {
-    var deff = arr[i + 1] - arr[i] // 2
-  if (deff > 1) { // true
-    for (let j = 1; j < deff ; j++) {
-      arr.splice(i + j,0,arr[i] + j);  
-    }  
-}
-}
-console.log(arr);
+// for (var i = 0 ; i < arr.length; i++) {
+//     var deff = arr[i + 1] - arr[i] // 2
+//   if (deff > 1) { // true
+//     for (let j = 1; j < deff ; j++) {
+//       arr.splice(i + j,0,arr[i] + j);  
+//     }  
+// }
+// }
+// console.log(arr);
 
 // fixed missing number in arry series by chatgpt
 
@@ -56,3 +75,101 @@ console.log(arr);
 // newArr.push(arr[arr.length - 1]);
 
 // console.log(newArr);
+
+
+//________missing number finding_________////
+
+// var myNum = [12,13,14,16,17,18,20]
+// for(var i = 0 ; i < myNum.length ; i++) // sab sy pehly simple loop chala 
+//   {
+// console.log(myNum[i])
+// }
+//__logic hum ny is sires me sy 2 number uthany hen pehli dafa loop chlny pr or is me bara number hamesha dosra wala hoga to bary number sy choty number ko minus krna hy answer me agr 2 hy matlab missing number hy agr answer 1 hy to missing number nhi hy condition ye bny gi 
+// " if (myNum [ i + 1] i 0 index pr hy jo ky 12 ly kr aaye ga jo 0 index pr number hoga + 1 ka matlab hoga  index ky 0 ky bad jo number hy us ko bhi ly ga is hisab sy yahan 13 hoga yani hum 0+1 jo second wala number hy us ko utha rhy hen phir aagy if (myNum [ i + 1] - myNum[i]  hum ny is 13 kw us ys pehly waly 0index waly number sy minus kr diya "
+//__ ye is tarha kam kry ga hum aagy waly ko pehly waly sy minus krwa rhy hen
+
+//____task_perform_finding a missing number
+// var myNum = [11,13,14,16,17,19,20]
+// for (var i = 0; i < myNum.length; i++) {
+//   if (myNum[i + 1] - myNum[i] > 1) {
+//  console.log(myNum[i] + 1); 
+//  // ye formula agr koi 1 number bech me sy missing ho to ye bata dyga sirf ek number ka diffence ho kiunke hum ny just +1 kia hy 
+//   }
+  
+// }
+
+//__ab agr sires me sy ek sath 2 ya 3 number missing ho jesy 12 , 17 , 20 to in ko find krny ka tariqa alg hy 
+
+// var myNum = [11,13,16,19,20]
+// for (var i = 0; i < myNum.length; i++) {
+//   var diff = myNum[i + 1] - myNum[i]; //lop ka variable jis me i ho hamesha lopp ky ander bnta hy
+//   if ( diff > 1) {
+//     for (var j = 1; j < diff; j++) {
+
+//       console.log(myNum[i] + j); 
+    
+      
+//     }
+   
+//   }
+  
+// }
+
+//___ missing number ki alg arry banani he to
+
+// var myNum = [11,13,16,19,20]
+// var missing = []
+// for (var i = 0; i < myNum.length; i++) {
+//   var diff = myNum[i + 1] - myNum[i]; //lop ka variable jis me i ho hamesha lopp ky ander bnta hy
+//   if ( diff > 1) {
+//     for (var j = 1; j < diff; j++) {
+//       missing.push(myNum[i] + j)
+      
+      
+//     }
+    
+//   }
+  
+// }
+// console.log(myNum);
+
+// console.log(missing);
+
+//____jahan sy number missing hy usy wahen dalna hy jahan sy missing hy 
+
+// var myNum = [11,13,16,19,20]
+// var missing = []
+// for (var i = 0; i < myNum.length; i++) {
+//   var diff = myNum[i + 1] - myNum[i]; //lop ka variable jis me i ho hamesha lopp ky ander bnta hy
+//   if ( diff > 1) {
+//     for (var j = 1; j < diff; j++) {
+//       missing.splice(myNum[i] + j)
+      
+      
+//     }
+    
+//   }
+  
+// }
+// console.log(myNum);
+
+// console.log(missing); 
+
+//_____ for set missing to there places________
+
+var myNum = [11, 13, 16, 19, 20];
+
+for (var i = 0; i < myNum.length - 1; i++) {
+  var diff = myNum[i + 1] - myNum[i];
+
+  if (diff > 1) {
+    for (var j = 1; j < diff; j++) {
+  
+      myNum.splice(i + j, 0, myNum[i] + j);
+    }
+    // jump to last inserted value
+    i += diff - 1;
+  }
+}
+
+console.log(myNum);
